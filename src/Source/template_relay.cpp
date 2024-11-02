fa3bb8e1dbc833878875ea69cb68659fe583c109
vint10tov <viktordesiatov@mail.ru>
2024-11-02 20:11:10 +0600

    rel001

--- src/Source/template_relay.cpp
+++ src/Source/template_relay.cpp
@@ -24,7 +24,7 @@ std::string replace_matches(std::string& input, UartUno* uartuno) {
         std::string inner = match[1].str();
         
         // Здесь добавляем логику замены
-        std::string replacement;
+        std::string replacement = "";
         if (is_number(inner)) { // Строка содержит число
             switch (atoi(inner.c_str())) {
             case 0:
@@ -77,13 +77,92 @@ std::string replace_matches(std::string& input, UartUno* uartuno) {
                         replacement = "OFF";
                 }
                 break;
+            case 7:
+                if (pars_relay.GET_type() == ParserUno::Type::OK) {
+                    if (pars_relay.GET_value(1))
+                        replacement = "0";
+                    else
+                        replacement = "1";
+                }
+                break;
+            case 8:
+                if (pars_relay.GET_type() == ParserUno::Type::OK) {
+                    if (pars_relay.GET_value(1))
+                        replacement = "OFF";
+                    else
+                        replacement = "ON";
+                }
+                break;
+            case 9:
+                if (pars_relay.GET_type() == ParserUno::Type::OK) {
+                    if (pars_relay.GET_value(3))
+                        replacement = "0";
+                    else
+                        replacement = "1";
+                }
+                break;
+            case 10:
+                if (pars_relay.GET_type() == ParserUno::Type::OK) {
+                    if (pars_relay.GET_value(3))
+                        replacement = "OFF";
+                    else
+                        replacement = "ON";
+                }
+                break;
+            case 11:
+                if (pars_relay.GET_type() == ParserUno::Type::OK) {
+                    if (pars_relay.GET_value(5))
+                        replacement = "0";
+                    else
+                        replacement = "1";
+                }
+                break;
+            case 12:
+                if (pars_relay.GET_type() == ParserUno::Type::OK) {
+                    if (pars_relay.GET_value(5))
+                        replacement = "OFF";
+                    else
+                        replacement = "ON";
+                }
+                break;
+            case 13:
+                if (pars_mod2_0.GET_type() == ParserUno::Type::OK) {
+                    if (pars_mod2_0.GET_value(0))
+                        replacement = "0";
+                    else
+                        replacement = "1";
+                }
+                break;
+            case 14:
+                if (pars_mod2_0.GET_type() == ParserUno::Type::OK) {
+                    if (pars_mod2_0.GET_value(0))
+                        replacement = "DEACT";
+                    else
+                        replacement = "ACT";
+                }
+                break;
+            case 15:
+                if (pars_mod2_1.GET_type() == ParserUno::Type::OK) {
+                    if (pars_mod2_1.GET_value(0))
+                        replacement = "0";
+                    else
+                        replacement = "1";
+                }
+                break;
+            case 16:
+                if (pars_mod2_1.GET_type() == ParserUno::Type::OK) {
+                    if (pars_mod2_1.GET_value(0))
+                        replacement = "DEACT";
+                    else
+                        replacement = "ACT";
+                }
+                break;
             default:
                 break;
             }
-        } else { // Строка не содержит число
+        } else { // Строка содержит не только число
             //
         }
-        
         // Заменяем найденное вхождение на новое значение
         result.replace(match.position(0), match.length(0), replacement);
     }
